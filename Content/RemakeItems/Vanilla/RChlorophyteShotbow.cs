935dbed6435249d748cd545f6f9acbcd5c56211e
HoCha113 <coslowkimberli185@gmail.com>
2024-03-20 20:24:22 +0800

    小型修改

--- Content/RemakeItems/Vanilla/RChlorophyteShotbow.cs
+++ Content/RemakeItems/Vanilla/RChlorophyteShotbow.cs
@@ -1,25 +1,19 @@
-﻿using CalamityOverhaul.Common;
-using CalamityOverhaul.Content.Projectiles.Weapons.Ranged.HeldProjs.Vanilla;
+﻿using CalamityOverhaul.Content.Projectiles.Weapons.Ranged.HeldProjs.Vanilla;
 using CalamityOverhaul.Content.RemakeItems.Core;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
-using Terraria.ModLoader;
 
 namespace CalamityOverhaul.Content.RemakeItems.Vanilla
 {
     internal class RChlorophyteShotbow : BaseRItem
     {
         public override int TargetID => ItemID.ChlorophyteShotbow;
-        public override bool FormulaSubstitution => false;
+        public override bool IsVanilla => true;
+        public override string TargetToolTipItemName => "Wap_ChlorophyteShotbow_Text";
         public override void SetDefaults(Item item) {
             item.SetHeldProj<ChlorophyteShotbowHeldProj>();
             item.useTime = 18;
             item.damage = 40;
         }
-        public override bool? On_Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) => false;
-        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) => CWRUtils.OnModifyTooltips(CWRMod.Instance, tooltips, CWRLocText.GetText("Wap_ChlorophyteShotbow_Text"));
     }
 }
