9ec0dd7b143adf6aab962f1a63466c7958f969f4
HoCha113 <coslowkimberli185@gmail.com>
2024-05-11 04:40:32 +0800

    修复一些问题

--- Content/Projectiles/Weapons/Magic/HeldProjs/WingmanHeldProj.cs
+++ Content/Projectiles/Weapons/Magic/HeldProjs/WingmanHeldProj.cs
@@ -1,7 +1,10 @@
 ﻿using CalamityMod.Items.Weapons.Magic;
+using CalamityMod.Projectiles.Magic;
 using CalamityOverhaul.Common;
 using CalamityOverhaul.Content.Items.Magic;
 using Terraria.ModLoader;
+using Terraria;
+using Terraria.Audio;
 
 namespace CalamityOverhaul.Content.Projectiles.Weapons.Magic.HeldProjs
 {
@@ -10,6 +13,7 @@ namespace CalamityOverhaul.Content.Projectiles.Weapons.Magic.HeldProjs
         public override string Texture => CWRConstant.Cay_Wap_Magic + "Wingman";
         public override int targetCayItem => ModContent.ItemType<Wingman>();
         public override int targetCWRItem => ModContent.ItemType<WingmanEcType>();
+        int fireIndex;
         public override void SetMagicProperty() {
             ShootPosToMouLengValue = 0;
             ShootPosNorlLengValue = 0;
@@ -28,7 +32,20 @@ namespace CalamityOverhaul.Content.Projectiles.Weapons.Magic.HeldProjs
         }
 
         public override int Shoot() {
-            return base.Shoot();
+            SoundStyle style = new SoundStyle("CalamityMod/Sounds/Item/MagnaCannonShot");
+            float rand = Main.rand.NextFloat(0.3f, 1.1f);
+            AmmoTypes = ModContent.ProjectileType<WingmanShot>();
+            if (++fireIndex > 2) {
+                style = new SoundStyle("CalamityMod/Sounds/Item/DeadSunExplosion");
+                AmmoTypes = ModContent.ProjectileType<WingmanGrenade>();
+                fireIndex = 0;
+            }
+            SoundEngine.PlaySound(style with { Volume = 0.6f }, Projectile.Center);
+            for (int i = 0; i < 3; i++) {
+                Projectile.NewProjectile(Source, GunShootPos, ShootVelocity.RotatedBy((-1 + i) * 0.1f * rand)
+                    , AmmoTypes, WeaponDamage, WeaponKnockback, Owner.whoAmI, 0);
+            }
+            return 0;
         }
     }
 }
