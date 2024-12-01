7485a162216f2d3899ff2431acbf46658db57444
Monomon <3152865050@qq.com>
2024-12-01 19:21:39 +0800

    按照wiki顺序肉前魔法武器前六把重置

---
+++ Content/Items/Magic/PlasmaRodEcType.cs
@@ -0,0 +1,59 @@
+﻿using CalamityMod.Items.Weapons.Magic;
+using CalamityOverhaul.Common;
+using CalamityOverhaul.Content.Projectiles.Weapons.Magic;
+using CalamityOverhaul.Content.RemakeItems.Core;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.Items.Magic
+{
+    internal class RPlasmaRod : BaseRItem
+    {
+        public override int TargetID => ModContent.ItemType<PlasmaRod>();
+        public override int ProtogenesisID => ModContent.ItemType<PlasmaRodEcType>();
+        public override string TargetToolTipItemName => "PlasmaRodEcType";
+        public override void SetDefaults(Item item) => item.SetHeldProj<PlasmaRodHeld>();
+    }
+
+    internal class PlasmaRodEcType : EctypeItem
+    {
+        public override string Texture => CWRConstant.Cay_Wap_Magic + "PlasmaRod";
+        public override void SetDefaults() {
+            Item.SetItemCopySD<PlasmaRod>();
+            Item.SetHeldProj<PlasmaRodHeld>();
+        }
+    }
+
+    internal class PlasmaRodHeld : BaseMagicGun
+    {
+        public override string Texture => CWRConstant.Cay_Wap_Magic + "PlasmaRod";
+        public override int targetCayItem => ModContent.ItemType<PlasmaRod>();
+        public override int targetCWRItem => ModContent.ItemType<PlasmaRodEcType>();
+        public override void SetMagicProperty() {
+            ShootPosToMouLengValue = -30;
+            ShootPosNorlLengValue = 0;
+            HandDistance = 30;
+            HandDistanceY = 0;
+            HandFireDistance = 30;
+            HandFireDistanceY = 0;
+            InOwner_HandState_AlwaysSetInFireRoding = true;
+            Onehanded = true;
+            GunPressure = 0;
+            ControlForce = 0;
+            Recoil = 0;
+        }
+
+        public override void FiringShoot() {
+            Projectile.NewProjectile(Source, GunShootPos + (InMousePos - Owner.Center).UnitVector() * 48f, ShootVelocity
+                , AmmoTypes, WeaponDamage, WeaponKnockback, Owner.whoAmI, 0);
+        }
+
+        public override void GunDraw(Vector2 drawPos, ref Color lightColor) {
+            float rot = DirSign > 0 ? MathHelper.PiOver4 : MathHelper.PiOver4 * 3;
+            Main.EntitySpriteDraw(TextureValue, drawPos, null, lightColor
+                , Projectile.rotation + rot, TextureValue.Size() / 2, Projectile.scale
+                , DirSign > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+        }
+    }
+}
---
+++ Content/Items/Magic/SkyGlazeEctype.cs
@@ -0,0 +1,68 @@
+﻿using CalamityMod.Items.Weapons.Magic;
+using CalamityOverhaul.Common;
+using CalamityOverhaul.Content.Projectiles.Weapons.Magic;
+using CalamityOverhaul.Content.RemakeItems.Core;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.Items.Magic
+{
+    internal class SkyGlazeEcType : EctypeItem
+    {
+        public override string Texture => CWRConstant.Cay_Wap_Magic + "SkyGlaze";
+        public override void SetDefaults() {
+            Item.SetItemCopySD<SkyGlaze>();
+            Item.SetHeldProj<SkyGlazeHeld>();
+        }
+    }
+
+    internal class RSkyGlaze : BaseRItem
+    {
+        public override int TargetID => ModContent.ItemType<SkyGlaze>();
+        public override int ProtogenesisID => ModContent.ItemType<SkyGlazeEcType>();
+        public override string TargetToolTipItemName => "SkyGlazeEcType";
+        public override void SetDefaults(Item item) => item.SetHeldProj<SkyGlazeHeld>();
+    }
+
+    internal class SkyGlazeHeld : BaseMagicGun
+    {
+        public override string Texture => CWRConstant.Cay_Wap_Magic + "SkyGlaze";
+        public override int targetCayItem => ModContent.ItemType<SkyGlaze>();
+        public override int targetCWRItem => ModContent.ItemType<SkyGlazeEcType>();
+        public override void SetMagicProperty() {
+            ShootPosToMouLengValue = -30;
+            ShootPosNorlLengValue = 0;
+            HandDistance = 30;
+            HandDistanceY = 0;
+            HandFireDistance = 30;
+            HandFireDistanceY = 0;
+            InOwner_HandState_AlwaysSetInFireRoding = true;
+            Onehanded = true;
+            GunPressure = 0;
+            ControlForce = 0;
+            Recoil = 0;
+        }
+
+        public virtual bool DrawingInfo => false;
+
+        public override void FiringShoot() {
+            for (int i = 3; i > 0; i--) {
+                int leftorright = (InMousePos - Owner.Center).X > 0 ? 1 : -1;
+                Vector2 starpos = Owner.Center + new Vector2(Main.rand.NextFloat(-120, 240) * leftorright, Main.rand.NextFloat(-600, -800));
+                Vector2 vel = (InMousePos - starpos).UnitVector() * 16f;
+                Terraria.Audio.SoundEngine.PlaySound(SoundID.Item8);
+                Projectile.NewProjectile(Source, starpos, vel
+                    , AmmoTypes, WeaponDamage, WeaponKnockback, Owner.whoAmI, 0);
+            }
+        }
+
+        public override void GunDraw(Vector2 drawPos, ref Color lightColor) {
+            float rot = DirSign > 0 ? MathHelper.PiOver4 : MathHelper.PiOver4 * 3;
+            Main.EntitySpriteDraw(TextureValue, drawPos, null, lightColor
+                , Projectile.rotation + rot, TextureValue.Size() / 2, Projectile.scale
+                , DirSign > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+        }
+    }
+}
