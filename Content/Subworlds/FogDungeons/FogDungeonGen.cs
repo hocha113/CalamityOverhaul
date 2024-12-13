53b8272a6be53d6bf1a0729e01d286c323663978
hocha <2188704314@qq.com>
2024-12-14 01:33:21 +0800

    开始地牢世界的制作，这个推送将意味着模组开始依赖SubworldLibrary

---
+++ Content/Subworlds/FogDungeons/FogDungeonGen.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.IO;
+using Terraria.WorldBuilding;
+
+namespace CalamityOverhaul.Content.Subworlds.FogDungeons
+{
+    internal class FogDungeonGen : GenPass
+    {
+        public FogDungeonGen() : base("Terrain", 1) { }
+
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration) {
+            
+        }
+    }
+}
