811512c902683e657ce535f656cd679821d27203
hocha <2188704314@qq.com>
2024-12-29 19:10:14 +0800

    欧米茄合成UI将使用新的纹理

--- Content/UIs/SupertableUIs/CraftingSlotHighlighter.cs
+++ Content/UIs/SupertableUIs/CraftingSlotHighlighter.cs
@@ -1,6 +1,7 @@
 ﻿using CalamityOverhaul.Common;
 using InnoVault.UIHandles;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent;
@@ -12,13 +13,13 @@ namespace CalamityOverhaul.Content.UIs.SupertableUIs
     {
         public static CraftingSlotHighlighter Instance;
         public override Texture2D Texture => CWRUtils.GetT2DValue("CalamityOverhaul/Assets/UIs/SupertableUIs/CallFull");
+        public static Asset<Texture2D> eyeAsset;
         public override float RenderPriority => 2;
         public override bool Active {
             get {
                 if (SupertableUI.Instance == null) {
                     return false;
                 }
-
                 return SupertableUI.Instance.Active;
             }
         }
@@ -26,9 +27,12 @@ namespace CalamityOverhaul.Content.UIs.SupertableUIs
         public Rectangle MainRec;
         public bool onMainP;
         public bool eyEBool;
-
         public override void Load() => Instance = this;
-        void ICWRLoader.UnLoadData() => Instance = null;
+        void ICWRLoader.LoadAsset() => eyeAsset = CWRUtils.GetT2DAsset("CalamityOverhaul/Assets/UIs/SupertableUIs/Eye");
+        void ICWRLoader.UnLoadData() {
+            Instance = null;
+            eyeAsset = null;
+        }
         public override void Update() {
             DrawPosition = mainUI.DrawPosition + new Vector2(460, 420);
             MainRec = new Rectangle((int)DrawPosition.X, (int)DrawPosition.Y, 30, 30);
@@ -48,12 +52,7 @@ namespace CalamityOverhaul.Content.UIs.SupertableUIs
             }
         }
         public override void Draw(SpriteBatch spriteBatch) {
-            Texture2D eye0 = CWRUtils.GetT2DValue("CalamityOverhaul/Assets/UIs/SupertableUIs/Eye0");
-            Texture2D eye1 = CWRUtils.GetT2DValue("CalamityOverhaul/Assets/UIs/SupertableUIs/Eye1");
-            spriteBatch.Draw(eye0, DrawPosition, null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-            if (eyEBool) {
-                spriteBatch.Draw(eye1, DrawPosition, null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-            }
+            spriteBatch.Draw(eyeAsset.Value, DrawPosition, CWRUtils.GetRec(eyeAsset.Value, eyEBool ? 1 : 0, 2), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
             if (mainUI.items != null) {
                 for (int i = 0; i < mainUI.items.Length; i++) {
                     if (mainUI.items[i] == null) {
@@ -63,7 +62,7 @@ namespace CalamityOverhaul.Content.UIs.SupertableUIs
                         mainUI.previewItems[i] = new Item();
                     }
                     if (mainUI.items[i].type != mainUI.previewItems[i].type && mainUI.items[i].type != ItemID.None && eyEBool) {
-                        Vector2 pos = mainUI.ArcCellPos(i) + new Vector2(-1, -2);
+                        Vector2 pos = mainUI.ArcCellPos(i) + new Vector2(-1, 0);
                         spriteBatch.Draw(Texture, pos, null, Color.White * 0.6f, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
                     }
                 }
