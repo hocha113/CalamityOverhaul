1ba5566d83f4ab22cab1a19fd84c085bb3bbed29
hocha <2188704314@qq.com>
2025-01-20 01:08:07 +0800

    一些更新

--- Content/NPCs/BrutalNPCs/BrutalEyeOfCthulhu/BrutalEyeOfCthulhuAI.cs
+++ Content/NPCs/BrutalNPCs/BrutalEyeOfCthulhu/BrutalEyeOfCthulhuAI.cs
@@ -1,52 +1,13 @@
-﻿using CalamityMod.Events;
-using CalamityMod.World;
-using CalamityOverhaul.Content.NPCs.Core;
-using Terraria;
+﻿using CalamityOverhaul.Content.NPCs.Core;
 using Terraria.ID;
 
 namespace CalamityOverhaul.Content.NPCs.BrutalNPCs.BrutalEyeOfCthulhu
 {
     internal class BrutalEyeOfCthulhuAI : NPCOverride
     {
-        private const float ProjectileOffset = 50f;
         public override int TargetID => NPCID.EyeofCthulhu;
-
-        private float lifeRatio => npc.life / (float)npc.lifeMax;
-
-        private bool bossRush => BossRushEvent.BossRushActive;
-
-        private bool masterMode => Main.masterMode || bossRush;
-
-        private bool death => CalamityWorld.death || bossRush;
-
-        private float phase2LifeRatio => masterMode ? 0.75f : 0.6f;
-
-        private float phase3LifeRatio => masterMode ? 0.4f : 0.3f;
-
-        private float finalPhaseRevLifeRatio => masterMode ? 0.2f : 0.15f;
-
-        private float penultimatePhaseDeathLifeRatio => masterMode ? 0.3f : 0.2f;
-
-        private float finalPhaseDeathLifeRatio => masterMode ? 0.15f : 0.1f;
-
-        private bool phase2 => lifeRatio < phase2LifeRatio;
-
-        private bool phase3 => lifeRatio < phase3LifeRatio;
-
-        private bool finalPhaseRev => lifeRatio < finalPhaseRevLifeRatio;
-
-        private bool penultimatePhaseDeath => lifeRatio < penultimatePhaseDeathLifeRatio;
-
-        private bool finalPhaseDeath => lifeRatio < finalPhaseDeathLifeRatio;
-
-        private float lineUpDist => death ? 15f : 20f;
-
-        private float servantAndProjectileVelocity => (death ? 8f : 6f) + (masterMode ? 2f : 0f);
-
-        private float enrageScale;
-
         public override void SetProperty() {
-            enrageScale = bossRush ? 1f : masterMode ? 0.5f : 0f;
+            
         }
 
         public override bool CanLoad() {
--- Content/NPCs/BrutalNPCs/BrutalKingSlime/BrutalKingSlimeAI.cs
+++ Content/NPCs/BrutalNPCs/BrutalKingSlime/BrutalKingSlimeAI.cs
@@ -6,29 +6,6 @@ namespace CalamityOverhaul.Content.NPCs.BrutalNPCs.BrutalKingSlime
     internal class BrutalKingSlimeAI : NPCOverride
     {
         public override int TargetID => NPCID.KingSlime;
-
-        //private bool bossRush => BossRushEvent.BossRushActive;
-
-        //private bool masterMode => Main.masterMode || bossRush;
-
-        //private bool death => CalamityWorld.death || bossRush;
-
-        //private bool crystalAlive = true;
-        //private bool blueCrystalAlive = false;
-        //private bool greenCrystalAlive = true;
-        //private float lifeRatio;
-        //private float lifeRatio2;
-        //private float teleportScale = 1f;
-        //private float teleportScaleSpeed;
-        //private float teleportGateValue;
-        //private bool teleporting = false;
-        //private bool teleported = false;
-        //private bool phase2;
-        //private bool phase3;
-        //private Color dustColor;
-        //private int setDamage;
-        //private NPC NPC;
-
         public override bool CanLoad() {
             return false;
         }
