d68c3760a6d24309e43b300b98394955ea16d12d
HoCha113 <2188704314@qq.com>
2025-09-22 17:30:46 +0800

    Add dye effect handling for items, projectiles, and NPCs
    Introduces DyeEffectHandle and related classes to manage dye shader effects for dust, projectiles, NPCs, items, and player visuals. Updates DyeMachineUI to use BeginDyeEffectForUI. Adds localization entries for DyeGlobalItem in all supported languages.

---
+++ Common/DyeEffectHandle.cs
@@ -0,0 +1,115 @@
+﻿using InnoVault.GameSystem;
+using InnoVault.PRT;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using static CalamityOverhaul.Common.DyeEffectHandle;
+
+namespace CalamityOverhaul.Common
+{
+    internal class DyeEffectHandle
+    {
+        /// <summary>
+        /// 染色体数据
+        /// </summary>
+        public static ArmorShaderData DyeShaderData { get; internal set; } = null;
+        public static bool IsDyeDustEffectActive { get; internal set; } = false;
+    }
+
+    internal class DyeGlobalDust : GlobalDust
+    {
+        public override void OnSpawn(Dust dust) {
+            if (!IsDyeDustEffectActive || DyeShaderData == null) {
+                return;
+            }
+            dust.shader = DyeShaderData;
+        }
+    }
+
+    internal class DyeGlobalPRT : GlobalPRT
+    {
+        public override void OnSpawn(BasePRT prt) {
+            if (!IsDyeDustEffectActive || DyeShaderData == null) {
+                return;
+            }
+            prt.shader = DyeShaderData;
+        }
+    }
+
+    internal class DyeGlobalProjectile : ProjOverride
+    {
+        public static bool IsUpdate { get; private set; } = false;
+        public override int TargetID => -1;//设置为-1，这样让这个修改节点运行在所有弹幕之上
+        public override bool AI() {
+            IsDyeDustEffectActive = IsUpdate = true;
+            int dyeItemID = projectile.CWR().DyeItemID;
+            if (dyeItemID > 0) {
+                DyeShaderData = GameShaders.Armor.GetShaderFromItemId(dyeItemID);
+            }
+            return true;
+        }
+
+        public override void PostAI() {
+            IsDyeDustEffectActive = IsUpdate = false;
+            DyeShaderData = null;
+        }
+    }
+
+    internal class DyeGlobalNPC : NPCOverride
+    {
+        public static bool IsUpdate { get; private set; } = false;
+        public override int TargetID => -1;//设置为-1，这样让这个修改节点运行在所有NPC之上
+        public override bool AI() {
+            IsDyeDustEffectActive = IsUpdate = true;
+            int dyeItemID = npc.CWR().DyeItemID;
+            if (dyeItemID > 0) {
+                DyeShaderData = GameShaders.Armor.GetShaderFromItemId(dyeItemID);
+            }
+            return true;
+        }
+        public override void PostAI() {
+            IsDyeDustEffectActive = IsUpdate = false;
+            DyeShaderData = null;
+        }
+    }
+
+    internal class DyeGlobalItem : ItemOverride
+    {
+        public static bool IsShootUpdate { get; private set; } = false;
+        public override int TargetID => -1;//设置为-1，这样让这个修改节点运行在所有物品之上
+        public override bool? On_Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source
+            , Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            IsDyeDustEffectActive = IsShootUpdate = true;
+            int dyeItemID = item.CWR().DyeItemID;
+            if (dyeItemID > 0) {
+                DyeShaderData = GameShaders.Armor.GetShaderFromItemId(dyeItemID);
+            }
+            return null;
+        }
+
+        public override void On_PostShoot(Item item, int whoAmI, int weaponDamage) {
+            IsDyeDustEffectActive = IsShootUpdate = false;
+            DyeShaderData = null;
+        }
+    }
+
+    internal class DyePlayer : PlayerOverride
+    {
+        public static bool IsMeleeEffectUpdate { get; private set; } = false;
+        public override int TargetItemID => ItemID.None;//设置为None，这样让这个修改节点运行在所有物品之上
+        public override bool On_PreEmitUseVisuals(Item item, ref Rectangle itemRectangle) {
+            IsDyeDustEffectActive = IsMeleeEffectUpdate = true;
+            int dyeItemID = item.CWR().DyeItemID;
+            if (dyeItemID > 0) {
+                DyeShaderData = GameShaders.Armor.GetShaderFromItemId(dyeItemID);
+            }
+            return true;
+        }
+
+        public override void On_PostEmitUseVisuals(Item item, ref Rectangle itemRectangle) {
+            IsDyeDustEffectActive = IsMeleeEffectUpdate = false;
+            DyeShaderData = null;
+        }
+    }
+}
--- Content/UIs/DyeMachineUI.cs
+++ Content/UIs/DyeMachineUI.cs
@@ -262,7 +262,7 @@ namespace CalamityOverhaul.Content.UIs
 
         protected virtual void DrawItemWithEffect(SpriteBatch spriteBatch, Color color) {
             if (Item.type > ItemID.None) {
-                Item.BeginDyeEffectForWorld(Item.CWR().DyeItemID);
+                Item.BeginDyeEffectForUI(Item.CWR().DyeItemID);
                 VaultUtils.SimpleDrawItem(spriteBatch, Item.type, DrawPosition + Size / 2, 64, (1.2f + hoverSengs * 0.2f) * sengs, 0f, color);
                 Item.EndDyeEffectForUI();
                 DrawItemStack(spriteBatch);
