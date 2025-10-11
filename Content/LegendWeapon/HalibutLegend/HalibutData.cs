a9cf06b64adde9dec1b0f118d15de448bb35b3ff
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-12 04:04:30 +0800

    Add methods for retrieving domain layer in HalibutData
    Introduced GetDomainLayer methods to HalibutData for obtaining the SeaDomainLayers value from the local player or a specified player. Updated XML documentation for clarity.

--- Content/LegendWeapon/HalibutLegend/HalibutData.cs
+++ Content/LegendWeapon/HalibutLegend/HalibutData.cs
@@ -8,10 +8,14 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend
     internal class HalibutData : LegendData
     {
         public override int TargetLevel => InWorldBossPhase.Halibut_Level();
+
+        /// <summary>
+        /// 获得武器成长等级
+        /// </summary>
         public static int GetLevel() => GetLevel(Main.LocalPlayer.GetItem());
 
         /// <summary>
-        /// 获得成长等级
+        /// 获得武器成长等级
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
@@ -30,6 +34,24 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend
                 return 12;
             }
             return cwrItem.LegendData.Level;
+        }
+
+        /// <summary>
+        /// 获得本地玩家的领域层数
+        /// </summary>
+        /// <returns></returns>
+        public static int GetDomainLayer() => GetDomainLayer(Main.LocalPlayer);
+
+        /// <summary>
+        /// 获得玩家的领域层数
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        public static int GetDomainLayer(Player player) {
+            if (player.TryGetOverride<HalibutPlayer>(out var halibutPlayer)) {
+                return halibutPlayer.SeaDomainLayers;
+            }
+            return 0;
         }
     }
 }
