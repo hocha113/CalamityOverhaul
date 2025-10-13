498c85a4c56beb3816395ce0f4355d6d41ec1368
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-13 14:50:57 +0800

    Add FishFallenStar skill and related asset
    Introduces the FishFallenStar skill class for HalibutLegend, unlocking with FallenStarfish and a 30s cooldown. Also adds the corresponding UI image asset.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishFallenStar.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishFallenStar : FishSkill
+    {
+        public override int UnlockFishID => ItemID.FallenStarfish;
+        public override int DefaultCooldown => 30;
+    }
+}
