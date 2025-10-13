1aa0bcc5d5bb79f202302a7f245b9435c5ff2b2d
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-13 15:09:57 +0800

    Add FishHunger skill and icon for HalibutLegend
    Introduces the FishHunger skill class for HalibutLegend, specifying its unlock fish and cooldown. Also adds the corresponding FishHunger icon image.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishHunger.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishHunger : FishSkill
+    {
+        public override int UnlockFishID => ItemID.Hungerfish;
+        public override int DefaultCooldown => 30;
+    }
+}
