8f85809244e4c9b9e6e17edf20488c65a417e366
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-13 14:05:21 +0800

    Add FishHarpy skill and related UI asset
    Introduced the FishHarpy skill class for HalibutLegend, unlocking with Harpyfish and a default cooldown of 30. Added corresponding FishHarpy.png UI asset.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishHarpy.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishHarpy : FishSkill
+    {
+        public override int UnlockFishID => ItemID.Harpyfish;
+        public override int DefaultCooldown => 30;
+    }
+}
