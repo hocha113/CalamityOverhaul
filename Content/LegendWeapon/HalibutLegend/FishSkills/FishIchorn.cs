692dd312df025203a52bb0086b7ec9b8014577a8
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-14 17:08:34 +0800

    Add FishIchorn skill and icon for HalibutLegend
    Introduces the FishIchorn skill class and its related global projectile effect, which applies the Ichor debuff on hit when active. Also adds the corresponding skill icon image.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishIchorn.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishIchorn : FishSkill
+    {
+        public override int UnlockFishID => ItemID.Ichorfish;
+        public override int DefaultCooldown => 120;
+    }
+
+    internal class FishIchornGlobalProj : GlobalProjectile
+    {
+        public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone) {
+            if (projectile.owner.TryGetPlayer(out var player) 
+                && FishSkill.GetT<FishIchorn>().Active(player)) {
+                target.AddBuff(BuffID.Ichor, 300);//在这个技能下攻击会附加灵液效果
+            }
+        }
+    }
+}
