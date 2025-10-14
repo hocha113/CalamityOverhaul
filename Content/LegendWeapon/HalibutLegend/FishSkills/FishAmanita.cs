a5787541bfa4cd762c30a000ec36b467a96bcc61
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-14 14:27:41 +0800

    Add FishAmanita skill and related UI asset
    Introduces the FishAmanita skill class for HalibutLegend, unlocking with AmanitaFungifin and a default cooldown of 60. Also adds the corresponding UI image asset.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishAmanita.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishAmanita : FishSkill
+    {
+        public override int UnlockFishID => ItemID.AmanitaFungifin;
+        public override int DefaultCooldown => 60;
+    }
+}
