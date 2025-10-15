a2e9cca269804388ba1534a0b23d6f9286306864
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-16 02:13:20 +0800

    Replace DemonicHellfish with Brimlish in scenario
    Updated the CalamitasCloneGift scenario to use Brimlish from CalamityMod instead of DemonicHellfish. Also revised related localization text to reference 硫磺火鱼 instead of 恶魔鱼 for consistency with the new item.

--- Content/LegendWeapon/HalibutLegend/ADV/Scenario/CalamitasCloneGift.cs
+++ Content/LegendWeapon/HalibutLegend/ADV/Scenario/CalamitasCloneGift.cs
@@ -1,5 +1,5 @@
-﻿using Terraria;
-using Terraria.ID;
+﻿using CalamityMod.Items.Fishing.BrimstoneCragCatches;
+using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
@@ -19,7 +19,7 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.ADV.Scenario
             R1 = this.GetLocalization(nameof(R1), () => "比目鱼");
             L0 = this.GetLocalization(nameof(L0), () => "一个影子……但影子不该有自己的意志。除非原型已经强大到开始分裂");
             L1 = this.GetLocalization(nameof(L1), () => "你击败的不是她，而是她的回声。真正的恐怖还在更深的地方等着");
-            L2 = this.GetLocalization(nameof(L2), () => "恶魔鱼，从灾厄的余烬中提取的。它在你手里低语着不该被听到的秘密");
+            L2 = this.GetLocalization(nameof(L2), () => "硫磺火鱼，从灾厄的余烬中提取的。它在你手里低语着不该被听到的秘密");
             L3 = this.GetLocalization(nameof(L3), () => "如果你开始听懂它在说什么......恭喜，你已经迈出了疯狂的第一步");
             L4 = this.GetLocalization(nameof(L4), () => "不过别担心，疯狂也是一种清醒，只是角度不同而已");
         }
@@ -34,7 +34,7 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.ADV.Scenario
         }
         public override void PreProcessSegment(DialogueBoxBase.DialoguePreProcessArgs args) {
             if (args.Index == 2) {
-                ADVRewardPopup.ShowReward(ItemID.DemonicHellfish, 1, null, appearDuration: 24, holdDuration: -1, giveDuration: 16, requireClick: true,
+                ADVRewardPopup.ShowReward(ModContent.ItemType<Brimlish>(), 1, null, appearDuration: 24, holdDuration: -1, giveDuration: 16, requireClick: true,
                     anchorProvider: () => {
                         var rect = DialogueUIRegistry.Current?.GetPanelRect() ?? Rectangle.Empty;
                         if (rect == Rectangle.Empty) {
