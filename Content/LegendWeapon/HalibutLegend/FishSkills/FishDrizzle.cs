3702f492e1a49a1f939c116fe4f2f655075f1977
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-16 18:59:16 +0800

    Add FishDrizzle skill and related assets
    Introduced the FishDrizzle skill class for HalibutLegend, including references to new Fire and FishDrizzle texture assets. This adds support for a new fish skill and its associated UI and masking graphics.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishDrizzle.cs
@@ -0,0 +1,13 @@
+﻿using CalamityMod.Items.Fishing.BrimstoneCragCatches;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishDrizzle : FishSkill
+    {
+        [VaultLoaden(CWRConstant.Masking)]
+        public static Texture2D Fire = null;//火焰的纹理灰度图，总共4*4帧，也就是四行四列的帧图
+        public override int UnlockFishID => ModContent.ItemType<DragoonDrizzlefish>();
+    }
+}
