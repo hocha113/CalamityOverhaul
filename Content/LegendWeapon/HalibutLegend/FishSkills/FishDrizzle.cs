518906ad93cf81cd9f2d2cce7a9c2fa9ecdea7b2
DrifitngSakura <2932595136@qq.com>
2025-10-17 13:06:30 +0800

    Modify FishDrizzle to reduce damage and speed up cooldown.

--- Content/LegendWeapon/HalibutLegend/FishSkills/FishDrizzle.cs
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishDrizzle.cs
@@ -16,7 +16,7 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
         [VaultLoaden(CWRConstant.Masking)]
         public static Texture2D Fire = null;//火焰的纹理灰度图，总共4*4帧，也就是四行四列的帧图
         public override int UnlockFishID => ModContent.ItemType<DragoonDrizzlefish>();
-        public override int DefaultCooldown => 720 - HalibutData.GetDomainLayer() * 36;
+        public override int DefaultCooldown => 480 - HalibutData.GetDomainLayer() * 24;
         public override int ResearchDuration => 60 * 16;
 
         private static int _drizzleVolleyIdSeed = 0;
@@ -330,7 +330,7 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
             SoundEngine.PlaySound(SoundID.Item74 with { Pitch = 0.2f, Volume = 0.9f }, Projectile.Center);
 
             Vector2 dir = (Main.MouseWorld - Projectile.Center).SafeNormalize(Vector2.UnitX);
-            int damage = (int)(StoredDamage * (1.2f + HalibutData.GetDomainLayer() * 0.25f));
+            int damage = (int)(StoredDamage * (0.95f + HalibutData.GetDomainLayer() * 0.2f));
 
             Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center + dir * 12f, dir * 0.1f,
                 ModContent.ProjectileType<DrizzleFirePillar>(), damage, StoredKnockback * 1.6f, Projectile.owner, Projectile.identity);
