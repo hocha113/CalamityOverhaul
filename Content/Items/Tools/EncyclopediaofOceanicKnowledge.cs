d8f189b61232e34e8f54ab5089b77d1a28a63fba
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-19 02:37:21 +0800

    Add Encyclopedia and Tome tool items
    Introduced EncyclopediaofOceanicKnowledge and TheForgottenTome tool items with corresponding textures. These new items are defined in the mod and linked to their respective image assets.

---
+++ Content/Items/Tools/EncyclopediaofOceanicKnowledge.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.Items.Tools
+{
+    internal class EncyclopediaofOceanicKnowledge : ModItem
+    {
+        public override string Texture => CWRConstant.Item + "Tools/EncyclopediaofOceanicKnowledge";
+    }
+}
---
+++ Content/Items/Tools/TheForgottenTome.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.Items.Tools
+{
+    internal class TheForgottenTome : ModItem
+    {
+        public override string Texture => CWRConstant.Item + "Tools/TheForgottenTome";
+    }
+}
