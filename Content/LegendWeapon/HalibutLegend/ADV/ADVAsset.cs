3c5dc32608edd6b077f6802c4a37ed1ac9865b7e
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-18 11:44:00 +0800

    Add Helen amaze and solemn ADV textures
    Added Helen_amaze.png and Helen_solemn.png assets and registered them in ADVAsset.cs for use in Halibut ADV. This expands available Helen ADV texture variants.

--- Content/LegendWeapon/HalibutLegend/ADV/ADVAsset.cs
+++ Content/LegendWeapon/HalibutLegend/ADV/ADVAsset.cs
@@ -7,6 +7,10 @@ namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.ADV
     {
         [VaultLoaden(CWRConstant.ADV + "Halibut/Helen")]
         public static Texture2D HelenADV = null;
+        [VaultLoaden(CWRConstant.ADV + "Halibut/Helen_amaze")]
+        public static Texture2D Helen_amazeADV = null;
+        [VaultLoaden(CWRConstant.ADV + "Halibut/Helen_solemn")]
+        public static Texture2D Helen_solemnADV = null;
         [VaultLoaden(CWRConstant.ADV + "SupCal/SupCal", startIndex: 0, arrayCount: 6)]
         public static IList<Texture2D> SupCalADV = null;
     }
