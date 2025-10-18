4376159546fb38c8e7c7b583f6e94b76c16904a6
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-19 01:06:32 +0800

    Add FishJewel skill and related asset
    Introduced the FishJewel skill class for HalibutLegend, unlocking with Jewelfish. Added corresponding FishJewel.png UI asset.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishJewel.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishJewel : FishSkill
+    {
+        public override int UnlockFishID => ItemID.Jewelfish;
+    }
+}
