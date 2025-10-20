912889643a1e0a2dd81d2e261f8b2a341a78e9f6
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-21 02:50:14 +0800

    Refactor ADVSave to use reflection for save/load
    Replaced manual serialization and deserialization of public bool fields with reflection-based logic in SaveData and LoadData. This reduces boilerplate and makes the code easier to maintain as fields are added or removed.

--- Content/ADV/ADVSave.cs
+++ Content/ADV/ADVSave.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ModLoader.IO;
+﻿using System.Reflection;
+using Terraria.ModLoader.IO;
 
 namespace CalamityOverhaul.Content.ADV
 {
@@ -35,158 +36,33 @@ namespace CalamityOverhaul.Content.ADV
         public bool SupCalChoseToFight;
         public bool SupCalMoonLordReward;
         public bool SupCalDefeat;
-        //任务相关字段
         public bool SupCalQuestAccepted;//玩家是否接受了任务
         public bool SupCalQuestDeclined;//玩家是否拒绝了任务
         public bool SupCalQuestReward;//玩家是否完成了任务（击杀了Providence）
         public bool SupCalQuestRewardSceneComplete;//任务完成后的奖励场景是否已播放
 
         public virtual TagCompound SaveData() {
-            TagCompound tag = new() {
-                ["FirstMet"] = FirstMet,
-                ["FishoilQuestDeclined"] = FishoilQuestDeclined,
-                ["FishoilQuestAccepted"] = FishoilQuestAccepted,
-                ["FishoilQuestCompleted"] = FishoilQuestCompleted,
-                ["FirstResurrectionWarning"] = FirstResurrectionWarning,
-                ["QueenBeeGift"] = QueenBeeGift,
-                ["SkeletronGift"] = SkeletronGift,
-                ["EyeOfCthulhuGift"] = EyeOfCthulhuGift,
-                ["KingSlimeGift"] = KingSlimeGift,
-                ["CrabulonGift"] = CrabulonGift,
-                ["PerforatorGift"] = PerforatorGift,
-                ["HiveMindGift"] = HiveMindGift,
-                ["WallOfFleshGift"] = WallOfFleshGift,
-                ["SlimeGodGift"] = SlimeGodGift,
-                ["CryogenGift"] = CryogenGift,
-                ["BrimstoneElementalGift"] = BrimstoneElementalGift,
-                ["AquaticScourgeGift"] = AquaticScourgeGift,
-                ["CalamitasCloneGift"] = CalamitasCloneGift,
-                ["PlanteraGift"] = PlanteraGift,
-                ["GolemGift"] = GolemGift,
-                ["MoonLordGift"] = MoonLordGift,
-                ["LeviathanGift"] = LeviathanGift,
-                ["PlaguebringerGift"] = PlaguebringerGift,
-                ["ProvidenceGift"] = ProvidenceGift,
-                ["DevourerOfGodsGift"] = DevourerOfGodsGift,
-                ["YharonGift"] = YharonGift,
-                ["SupremeCalamitasGift"] = SupremeCalamitasGift,
-                ["FirstMetSupCal"] = FirstMetSupCal,
-                ["SupCalChoseToFight"] = SupCalChoseToFight,
-                ["SupCalMoonLordReward"] = SupCalMoonLordReward,
-                ["SupCalDefeat"] = SupCalDefeat,
-                ["SupCalQuestAccepted"] = SupCalQuestAccepted,
-                ["SupCalQuestDeclined"] = SupCalQuestDeclined,
-                ["SupCalQuestReward"] = SupCalQuestReward,
-                ["SupCalQuestRewardSceneComplete"] = SupCalQuestRewardSceneComplete,
-            };
+            TagCompound tag = [];
+            
+            //使用反射自动保存所有公共字段
+            FieldInfo[] fields = GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (FieldInfo field in fields) {
+                if (field.FieldType != typeof(bool)) {
+                    continue;
+                }
+                tag[field.Name] = field.GetValue(this);
+            }
+            
             return tag;
         }
 
         public virtual void LoadData(TagCompound tag) {
-            if (tag.TryGet("FirstMet", out bool firstMet)) {
-                FirstMet = firstMet;
-            }
-            if (tag.TryGet("FishoilQuestDeclined", out bool fishoilQuestDeclined)) {
-                FishoilQuestDeclined = fishoilQuestDeclined;
-            }
-            if (tag.TryGet("FishoilQuestAccepted", out bool fishoilQuestAccepted)) {
-                FishoilQuestAccepted = fishoilQuestAccepted;
-            }
-            if (tag.TryGet("FishoilQuestCompleted", out bool fishoilQuestCompleted)) {
-                FishoilQuestCompleted = fishoilQuestCompleted;
-            }
-            if (tag.TryGet("FirstResurrectionWarning", out bool firstResurrectionWarning)) {
-                FirstResurrectionWarning = firstResurrectionWarning;
-            }
-            if (tag.TryGet("QueenBeeGift", out bool q)) {
-                QueenBeeGift = q;
-            }
-            if (tag.TryGet("SkeletronGift", out bool s)) {
-                SkeletronGift = s;
-            }
-            if (tag.TryGet("EyeOfCthulhuGift", out bool e)) {
-                EyeOfCthulhuGift = e;
-            }
-            if (tag.TryGet("KingSlimeGift", out bool ks)) {
-                KingSlimeGift = ks;
-            }
-            if (tag.TryGet("CrabulonGift", out bool c)) {
-                CrabulonGift = c;
-            }
-            if (tag.TryGet("PerforatorGift", out bool p)) {
-                PerforatorGift = p;
-            }
-            if (tag.TryGet("HiveMindGift", out bool h)) {
-                HiveMindGift = h;
-            }
-            if (tag.TryGet("WallOfFleshGift", out bool w)) {
-                WallOfFleshGift = w;
-            }
-            if (tag.TryGet("SlimeGodGift", out bool sg)) {
-                SlimeGodGift = sg;
-            }
-            if (tag.TryGet("CryogenGift", out bool cr)) {
-                CryogenGift = cr;
-            }
-            if (tag.TryGet("BrimstoneElementalGift", out bool be)) {
-                BrimstoneElementalGift = be;
-            }
-            if (tag.TryGet("AquaticScourgeGift", out bool as_)) {
-                AquaticScourgeGift = as_;
-            }
-            if (tag.TryGet("CalamitasCloneGift", out bool cc)) {
-                CalamitasCloneGift = cc;
-            }
-            if (tag.TryGet("PlanteraGift", out bool pl)) {
-                PlanteraGift = pl;
-            }
-            if (tag.TryGet("GolemGift", out bool go)) {
-                GolemGift = go;
-            }
-            if (tag.TryGet("MoonLordGift", out bool ml)) {
-                MoonLordGift = ml;
-            }
-            if (tag.TryGet("LeviathanGift", out bool lv)) {
-                LeviathanGift = lv;
-            }
-            if (tag.TryGet("PlaguebringerGift", out bool pb)) {
-                PlaguebringerGift = pb;
-            }
-            if (tag.TryGet("ProvidenceGift", out bool pr)) {
-                ProvidenceGift = pr;
-            }
-            if (tag.TryGet("DevourerOfGodsGift", out bool dog)) {
-                DevourerOfGodsGift = dog;
-            }
-            if (tag.TryGet("YharonGift", out bool yh)) {
-                YharonGift = yh;
-            }
-            if (tag.TryGet("SupremeCalamitasGift", out bool sc)) {
-                SupremeCalamitasGift = sc;
-            }
-            if (tag.TryGet("FirstMetSupCal", out bool fc)) {
-                FirstMetSupCal = fc;
-            }
-            if (tag.TryGet("SupCalChoseToFight", out bool scf)) {
-                SupCalChoseToFight = scf;
-            }
-            if (tag.TryGet("SupCalMoonLordReward", out bool scmr)) {
-                SupCalMoonLordReward = scmr;
-            }
-            if (tag.TryGet("SupCalDefeat", out bool scd)) {
-                SupCalDefeat = scd;
-            }
-            if (tag.TryGet("SupCalQuestAccepted", out bool scqa)) {
-                SupCalQuestAccepted = scqa;
-            }
-            if (tag.TryGet("SupCalQuestDeclined", out bool scqd)) {
-                SupCalQuestDeclined = scqd;
-            }
-            if (tag.TryGet("SupCalQuestReward", out bool scqr)) {
-                SupCalQuestReward = scqr;
-            }
-            if (tag.TryGet("SupCalQuestRewardSceneComplete", out bool scqrc)) {
-                SupCalQuestRewardSceneComplete = scqrc;
+            //使用反射自动加载所有公共字段
+            FieldInfo[] fields = GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (FieldInfo field in fields) {
+                if (tag.TryGet(field.Name, out bool value)) {
+                    field.SetValue(this, value);
+                }
             }
         }
     }
