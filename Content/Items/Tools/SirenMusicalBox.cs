10aa7b35e76a9a0b63321991ba4bd6e75f3b7709
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-21 03:24:48 +0800

    Add Siren Musical Box item, tile, and assets
    Introduced the Siren Musical Box item, its associated tile, and tile processor in the codebase. Added corresponding sprite and music assets to support the new item.

---
+++ Content/Items/Tools/SirenMusicalBox.cs
@@ -0,0 +1,20 @@
+﻿using InnoVault.TileProcessors;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul.Content.Items.Tools
+{
+    internal class SirenMusicalBox : ModItem
+    {
+        public override string Texture => CWRConstant.Item + "Tools/SirenMusicalBox";
+    }
+
+    internal class SirenMusicalBoxTile : ModTile
+    {
+        public override string Texture => CWRConstant.Item + "Tools/SirenMusicalBoxTile";
+    }
+
+    internal class SirenMusicalBoxTP : TileProcessor
+    {
+        public override int TargetTileID => ModContent.TileType<SirenMusicalBoxTile>();
+    }
+}
