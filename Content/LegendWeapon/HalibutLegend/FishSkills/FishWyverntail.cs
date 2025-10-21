23513239d307938f89ff5eaf3a361acc21f6f7ab
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-21 14:50:18 +0800

    Add FishWyverntail skill and UI asset
    Introduces the FishWyverntail skill class for HalibutLegend and adds its corresponding UI image. The skill unlocks with the Wyverntail item.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishWyverntail.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishWyverntail : FishSkill
+    {
+        public override int UnlockFishID => ItemID.Wyverntail;
+    }
+}
