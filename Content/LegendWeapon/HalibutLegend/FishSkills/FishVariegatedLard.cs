80c767f1e57401b2faa16abe90f6b45ccfb1b2d4
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-27 15:35:52 +0800

    update

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishVariegatedLard.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishVariegatedLard : FishSkill
+    {
+        public override int UnlockFishID => ItemID.VariegatedLardfish;
+    }
+}
