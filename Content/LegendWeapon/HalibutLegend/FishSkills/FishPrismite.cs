0431bb532754c186b068ad6d835c7719e310fafc
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-10-27 13:44:16 +0800

    Add FishPrismite skill and related image resource
    Introduced a new FishPrismite skill inheriting from FishSkill, with an override property UnlockFishID linked to Prismite. Added a corresponding FishPrismite.png image resource to support the new functionality.

---
+++ Content/LegendWeapon/HalibutLegend/FishSkills/FishPrismite.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ID;
+
+namespace CalamityOverhaul.Content.LegendWeapon.HalibutLegend.FishSkills
+{
+    internal class FishPrismite : FishSkill
+    {
+        public override int UnlockFishID => ItemID.Prismite;
+    }
+}
