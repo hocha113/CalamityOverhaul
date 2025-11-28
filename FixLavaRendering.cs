f7b8133caf11deb6d25cbdfca6be44d31872d7ba
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-11-28 20:22:28 +0800

    Create FixLavaRendering.cs

---
+++ FixLavaRendering.cs
@@ -0,0 +1,13 @@
+﻿using CalamityMod.Systems;
+using System.Linq;
+using Terraria.ModLoader;
+
+namespace CalamityOverhaul
+{
+    internal class FixLavaRendering : ICWRLoader
+    {
+        void ICWRLoader.SetupData() {
+            LavaRendering.instance.WaterStyleMaxCount = ModContent.GetContent<ModWaterStyle>().Count() + LoaderManager.Get<WaterStylesLoader>().VanillaCount;
+        }
+    }
+}
