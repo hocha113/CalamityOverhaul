685aa0137239b9fbb65da5da838313ff999e320a
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-11-28 20:33:16 +0800

    Update FixLavaRendering.cs

--- FixLavaRendering.cs
+++ FixLavaRendering.cs
@@ -1,13 +1,31 @@
-﻿using CalamityMod.Systems;
+﻿using CalamityOverhaul.Common;
 using System.Linq;
+using System.Reflection;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
 
 namespace CalamityOverhaul
 {
     internal class FixLavaRendering : ICWRLoader
     {
+        //对此我是无语的，就帮灾官擦一次屁股
         void ICWRLoader.SetupData() {
-            LavaRendering.instance.WaterStyleMaxCount = ModContent.GetContent<ModWaterStyle>().Count() + LoaderManager.Get<WaterStylesLoader>().VanillaCount;
+            if (CWRMod.Instance.calamity == null) {
+                return;
+            }
+            var type = ModGanged.GetTargetTypeInStringKey(AssemblyManager.GetLoadableTypes(CWRMod.Instance.calamity.Code), "LavaRendering");
+            if (type == null) {
+                return;
+            }
+            var fid = type.GetField("instance", BindingFlags.Static | BindingFlags.Public);
+            if (fid == null) {
+                return;
+            }
+            var fid2 = type.GetField("WaterStyleMaxCount", BindingFlags.Instance | BindingFlags.Public);
+            if (fid2 == null) {
+                return;
+            }
+            fid2.SetValue(fid.GetValue(null), ModContent.GetContent<ModWaterStyle>().Count() + LoaderManager.Get<WaterStylesLoader>().VanillaCount);
         }
     }
 }
