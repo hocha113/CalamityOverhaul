2d1aa36fd789a16689f0577ff4aa2ec70bee8712
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-11-29 18:58:21 +0800

    Create OldDukeCampsite.cs

---
+++ Content/ADV/Scenarios/Abysses/OldDukes/Campsites/OldDukeCampsite.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace CalamityOverhaul.Content.ADV.Scenarios.Abysses.OldDukes.Campsites
+{
+    /// <summary>
+    /// 老公爵营地
+    /// </summary>
+    internal class OldDukeCampsite
+    {
+        //反射加载老公爵贴图，以便在ADV场景中使用，总共七帧，一般只使用前六帧，因为第七帧是张嘴动画
+        [VaultLoaden("@CalamityMod/NPCs/OldDuke/")]
+        public static Texture2D OldDuke;
+    }
+}
