76eb0a5ec71141f0e1056a1df25e73ec6bacb217
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-05-11 00:23:51 +0800

    update

--- Content/Cyberwares/Implementation/Sandevistans/SandevistansItem.cs
+++ Content/Cyberwares/Implementation/Sandevistans/SandevistansItem.cs
@@ -8,8 +8,6 @@ namespace CalamityOverhaul.Content.Cyberwares.Implementation.Sandevistans
     /// </summary>
     internal class SandevistansItem : BaseCyberware
     {
-        public override string Texture => CWRConstant.Item_Other + "SandevistansItem";
-
         public override CyberwareSlotCategory SlotCategory => CyberwareSlotCategory.NervousSystem;
 
         public override int CapacityCost => 3;
