9b05169b055dfed4da10db0025ec05929e1378d6
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-05-16 16:44:34 +0800

    update

--- Content/LegendWeapon/SHPCLegend/Cyberspaces/CyberTraceBeamProj.cs
+++ Content/LegendWeapon/SHPCLegend/Cyberspaces/CyberTraceBeamProj.cs
@@ -510,10 +510,6 @@ namespace CalamityOverhaul.Content.LegendWeapon.SHPCLegend.Cyberspaces
             SHPCModificationSystem.ForEachModule(Main.player[Projectile.owner], mod => mod.OnBeamHitNPC(this, target, hit, damageDone));
         }
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers) {
-            if (Projectile.localAI[0] == 0) {
-                Projectile.localAI[0] = Projectile.damage;
-            }
-
             int hitCount = Projectile.numHits;
             float multiplier = Math.Max(0.5f, (float)Math.Pow(0.7f, hitCount));
             modifiers.FinalDamage *= multiplier;
