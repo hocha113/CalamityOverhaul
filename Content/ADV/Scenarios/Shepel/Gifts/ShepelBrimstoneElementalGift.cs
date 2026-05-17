596c42451c7b8db196b817d8c2fd8c133a3797aa
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-05-17 16:57:57 +0800

    update

--- Content/ADV/Scenarios/Shepel/Gifts/ShepelBrimstoneElementalGift.cs
+++ Content/ADV/Scenarios/Shepel/Gifts/ShepelBrimstoneElementalGift.cs
@@ -39,7 +39,7 @@ namespace CalamityOverhaul.Content.ADV.Scenarios.Shepel.Gifts
 
         public override void PreProcessSegment(DialoguePreProcessArgs args) {
             if (args.Index == 2) {
-                ADVRewardPopup.ShowReward(ModContent.ItemType<ScorchBarrelModule>(), 1, null,
+                ADVRewardPopup.ShowReward(ModContent.ItemType<MagmaVentBarrelModule>(), 1, null,
                     appearDuration: 24, holdDuration: -1, giveDuration: 16, requireClick: true,
                     anchorProvider: () => {
                         var rect = DialogueUIRegistry.Current?.GetPanelRect() ?? Rectangle.Empty;
