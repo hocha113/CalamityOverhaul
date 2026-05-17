2ac01246853105edb54b33de4158915ff4e0e574
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-05-17 17:23:29 +0800

    update

--- Content/ADV/Scenarios/Shepel/Gifts/ShepelCalamitasCloneGift.cs
+++ Content/ADV/Scenarios/Shepel/Gifts/ShepelCalamitasCloneGift.cs
@@ -37,7 +37,7 @@ namespace CalamityOverhaul.Content.ADV.Scenarios.Shepel.Gifts
 
         public override void PreProcessSegment(DialoguePreProcessArgs args) {
             if (args.Index == 2) {
-                ADVRewardPopup.ShowReward(ModContent.ItemType<ReflectionBarrelModule>(), 1, null,
+                ADVRewardPopup.ShowReward(ModContent.ItemType<ScorchBarrelModule>(), 1, null,
                     appearDuration: 24, holdDuration: -1, giveDuration: 16, requireClick: true,
                     anchorProvider: () => {
                         var rect = DialogueUIRegistry.Current?.GetPanelRect() ?? Rectangle.Empty;
