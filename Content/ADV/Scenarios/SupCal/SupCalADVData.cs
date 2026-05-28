ad9951aa695085a674256ecca2121153c2fd4969
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-05-28 22:32:21 +0800

    update

--- Content/ADV/Scenarios/SupCal/SupCalADVData.cs
+++ Content/ADV/Scenarios/SupCal/SupCalADVData.cs
@@ -1,7 +1,7 @@
 ﻿namespace CalamityOverhaul.Content.ADV.Scenarios.SupCal
 {
     /// <summary>
-    /// 至高卡拉米蒂剧情线的存档数据
+    /// 硫火女巫剧情线的存档数据
     /// </summary>
     public class SupCalADVData : ADVDataModule
     {
