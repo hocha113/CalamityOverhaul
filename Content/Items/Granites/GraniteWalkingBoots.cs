689316a811c25b9d7716a3ffe0d60916d6390a4d
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-06-09 22:46:32 +0800

    update

--- Content/Items/Granites/GraniteWalkingBoots.cs
+++ Content/Items/Granites/GraniteWalkingBoots.cs
@@ -1,5 +1,4 @@
-﻿using CalamityOverhaul.Common;
-using CalamityOverhaul.Content.PRTTypes;
+﻿using CalamityOverhaul.Content.PRTTypes;
 using InnoVault.PRT;
 using System;
 using Terraria;
--- Content/Items/Marbles/MarbleHuntingKnife.cs
+++ Content/Items/Marbles/MarbleHuntingKnife.cs
@@ -1,5 +1,4 @@
-﻿using CalamityOverhaul.Common;
-using CalamityOverhaul.Content.PRTTypes;
+﻿using CalamityOverhaul.Content.PRTTypes;
 using InnoVault.GameContent.BaseEntity;
 using InnoVault.PRT;
 using Microsoft.Xna.Framework.Graphics;
