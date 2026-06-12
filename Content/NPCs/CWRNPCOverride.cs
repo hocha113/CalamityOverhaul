a403f5349f03b33673a37be3b200b24820affcfe
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-06-12 23:39:05 +0800

    Update CWRNPCOverride.cs

--- Content/NPCs/CWRNPCOverride.cs
+++ Content/NPCs/CWRNPCOverride.cs
@@ -13,10 +13,6 @@ namespace CalamityOverhaul.Content.NPCs
             if (!CWRServerConfig.Instance.BiologyOverhaul) {
                 return false;
             }
-            //安装了Calamity时限制仅在复仇/死亡/Boss急速模式下启用，防止与其他mod的AI覆盖冲突
-            if (CWRRef.Has) {
-                return CWRRef.GetRevengeMode() || CWRRef.GetDeathMode() || CWRRef.GetBossRushActive();
-            }
             return true;
         }
 
