3307fd29029192dad6b5a63097adab1288b391d6
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-06-13 23:25:06 +0800

    Update Victor.cs

--- Content/Cyberwares/Victors/Victor.cs
+++ Content/Cyberwares/Victors/Victor.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using Terraria;
-using Terraria.Audio;
 using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
 using Terraria.ID;
