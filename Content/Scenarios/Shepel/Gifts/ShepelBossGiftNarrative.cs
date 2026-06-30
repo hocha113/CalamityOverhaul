13431626ce7d273fae971d16ff04f43b68620690
HoCha113 <2188704314@qq.com>
2026-06-30 18:33:25 +0800

    Update ShepelBossGiftNarrative.cs

--- Content/Scenarios/Shepel/Gifts/ShepelBossGiftNarrative.cs
+++ Content/Scenarios/Shepel/Gifts/ShepelBossGiftNarrative.cs
@@ -1,12 +1,10 @@
 ﻿using CalamityOverhaul.Content.LegendWeapon.SHPCLegend;
 using CalamityOverhaul.Content.Narrative.Common;
-using CalamityOverhaul.Content.Scenarios.Helen.Gifts;
 using InnoVault.Narrative.Core;
 using InnoVault.Narrative.Runtime;
 using System;
 using System.Collections.Generic;
 using Terraria;
-using Terraria.ModLoader;
 
 namespace CalamityOverhaul.Content.Scenarios.Shepel.Gifts
 {
