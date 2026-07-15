27d41eaeba89a383c4ba0f78c752e9dd9c1dc107
HoCha113 <2188704314@qq.com>
2026-07-16 00:40:52 +0800

    Update HimayaFullBodyPortrait.cs

--- Content/Scenarios/Himaya/HimayaFullBodyPortrait.cs
+++ Content/Scenarios/Himaya/HimayaFullBodyPortrait.cs
@@ -1,6 +1,14 @@
-﻿namespace CalamityOverhaul.Content.Scenarios.Himaya
+﻿using CalamityOverhaul.Content.Narrative.Presentation;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CalamityOverhaul.Content.Scenarios.Himaya
 {
-    internal class HimayaFullBodyPortrait
+    internal class HimayaFullBodyPortrait : FullBodyPortraitBase
     {
+        public override string PortraitKey => "HimayaFullBody";
+
+        protected override void OnDraw(SpriteBatch spriteBatch, float alpha) {
+            
+        }
     }
 }
